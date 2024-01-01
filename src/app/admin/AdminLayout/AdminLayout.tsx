dac4637c58f84e882e3a73d609d8f4a7d1bb76c6
mhmAdmin1122 <hasaanad12min@gmail.com>
2024-01-01 23:18:27 +0500

    admin panel security creted

--- src/app/admin/AdminLayout/AdminLayout.tsx
+++ src/app/admin/AdminLayout/AdminLayout.tsx
@@ -1,14 +1,52 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import SideBar from "../AdminComponents/SideBar";
 
 const AdminLayout = ({ children }: any) => {
+  const [condition, setCondition] = useState(false);
+  const [password, setPassword] = useState(0);
+
+  const handleOnSubmitCheck = (e: any) => {
+    if (password == 7692) {
+      setCondition(true);
+    } else {
+      setCondition(false);
+    }
+    console.log(condition)
+  };
+
   return (
-    <div className="flex">
-      <SideBar />
-      <div className="adminBodyBox w-[80%] p-[30px] h-screen">
-        <div className="bodyboxform w-full bg-[#00ff] text-[#fff] h-full px-[30px] py-[20px] overflow-scroll overflow-x-hidden rounded-md shadow-lg shadow-gray-500">{children}</div>
-      </div>
-    </div>
+    <>
+      {condition ? (
+        <div className="flex">
+          <SideBar />
+          <div className="adminBodyBox w-[80%] p-[30px] h-screen">
+            <div className="bodyboxform w-full bg-[#00ff] text-[#fff] h-full px-[30px] py-[20px] overflow-scroll overflow-x-hidden rounded-md shadow-lg shadow-gray-500">
+              {children}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <form
+          className="securityBox bg-[#ececec] px-[20px] py-10px min-h-screen max-h-screen flex items-center justify-center"
+          onSubmit={handleOnSubmitCheck}
+        >
+          <div className="bg-[#fff] text-[#000] pl-[30px] rounded shadow-md shadow-gray-300 border-[1px] border-gray-200">
+            <input
+              type="number"
+              name="password"
+              placeholder="Enter Password"
+              className="outline-none"
+              value={password}
+              onChange={(e: any) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <button className="px-[15px] py-[12px]">Check</button>
+          </div>
+        </form>
+      )}
+    </>
   );
 };
 
